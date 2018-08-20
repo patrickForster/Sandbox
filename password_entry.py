7b5471cb8d24561e591b86f442acf80212030eee
patrickForster <30456282+patrickForster@users.noreply.github.com>
2018-08-20 13:27:18 +1000

    delete file because it was added incorrectly

--- password_entry.py
+++
@@ -1,9 +0,0 @@
-MIN_LENGTH = 4
-print("Please enter a valid password")
-print("Your password must be at least", MIN_LENGTH, "characters long")
-password = input("> ")
-while len(password) < MIN_LENGTH:
-    print("Not enough characters!")
-    password = input("> ")
-hidden_password = len(password)*'*'
-print("Your {}-character password is valid: {}".format(len(password), hidden_password))
